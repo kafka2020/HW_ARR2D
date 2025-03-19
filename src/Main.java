049573a3bbb1998af6c46adcb4e3eb0faaf5337d
Andrew S <>
2025-03-19 22:45:18 +0300

    3 step Create method createMatrix

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Main {
     private static final int SIZE = 8;
 
@@ -10,11 +12,24 @@ public class Main {
         printMatrix(matrix);
 
 //        Разворот матрицы;
-        int[][] result = rotateMatrix(matrix);
+//        int[][] result = rotateMatrix(matrix);
 
 //        Вывод матрицы после поворота на 90 градусов.
-        System.out.println("Перевернутая матрица");
-        printMatrix(result);
+//        System.out.println("Перевернутая матрица");
+//        printMatrix(result);
+    }
+
+    private static int[][] createMatrix(int rows, int cols) {
+        int[][] matrix = new int[rows][cols];
+        Random random = new Random();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = random.nextInt(256);
+            }
+        }
+
+        return matrix;
     }
 
     private static void printMatrix(int[][] matrix) {
