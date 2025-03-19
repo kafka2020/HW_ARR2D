56a82c0d3a33004bce45120a2271019e1be27d2a
Andrew S <>
2025-03-19 22:10:30 +0300

    2 step Create method printMatrix

--- src/Main.java
+++ src/Main.java
@@ -16,4 +16,13 @@ public class Main {
         System.out.println("Перевернутая матрица");
         printMatrix(result);
     }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.printf("%4d", matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
